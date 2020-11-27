848800fbdcbfe73871c1ac7bc224e02d7166e67a
AlirezaChaviwala <48858980+AlirezaChaviwala@users.noreply.github.com>
2020-11-27 23:12:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,82 @@
+function setAttr(val1, val2, element) {
+    for (var i = 0; i < val1.length; i++) {
+        element.setAttribute(val1[i], val2[i]);
+    }
+}
+
+let header = document.createElement('h1');
+setAttr(['class'], ['jumbotron bg-transparent text-center'], header)
+header.innerText = 'Rest Countries using Async/Await';
+document.body.append(header);
+
+
+
+async function getRestData() {
+    try {
+        var restResponse = await fetch('https://restcountries.eu/rest/v2/all');
+        var restData = await restResponse.json();
+
+        let container = document.createElement('div');
+        setAttr(['class'], ['container'], container);
+        document.body.append(container);
+
+        let row = document.createElement('div');
+        setAttr(['class'], ['row'], row);
+        container.append(row);
+        for (let i = 0; i < restData.length; i++) {
+            let col = document.createElement('div');
+            setAttr(['class'], ['col-lg-4 col-sm-12'], col);
+            row.append(col);
+
+            let card = document.createElement('div');
+            setAttr(['class'], ['card mb-3'], card);
+            col.append(card);
+
+            let cardheader = document.createElement('div');
+            setAttr(['class'], ['card-header bg-dark text-white'], cardheader);
+            cardheader.innerText = `${restData[i].name}`;
+            card.append(cardheader);
+
+            let cardbody = document.createElement('div');
+            setAttr(['class'], ['card-body d-flex flex-column align-items-center'], cardbody);
+            card.append(cardbody);
+            let flag = document.createElement('img');
+            flag.src = restData[i].flag;
+            setAttr(['class', 'style'], ['m-3 card-img', 'height:150px;'], flag)
+            cardbody.append(flag);
+            let capital = document.createElement('p');
+            capital.innerText = `Capital: ${restData[i].capital}`
+            cardbody.append(capital);
+            let region = document.createElement('p');
+            region.innerText = `Region: ${restData[i].region}`
+            cardbody.append(region);
+            let countryCode = document.createElement('p');
+            countryCode.innerText = `Country Code: ${restData[i].alpha3Code}`
+            cardbody.append(countryCode);
+
+            let butt = document.createElement('button');
+            setAttr(['type', 'class', 'data-toggle', 'data-placement'], ['button', 'btn btn-primary', 'tooltip', 'top'], butt);
+            butt.innerText = 'Click for Weather';
+            cardbody.append(butt);
+
+            try {
+                var weatherResponse = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${restData[i].capital}&units=metric&appid=9b23c74551ba0264b9587dc8837da0e3`)
+                var weatherData = await weatherResponse.json();
+                butt.title = `Temperature: ${weatherData.main.temp}
+                Feels like: ${weatherData.main.feels_like}
+                Max. Temperature: ${weatherData.main.temp_max}
+                Min. Temperature: ${weatherData.main.temp_min}
+                Humidity: ${weatherData.main.humidity}`
+
+            } catch (err2) {
+                butt.title = `Weather not available for this city`
+                continue;
+            }
+
+        }
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+getRestData();
